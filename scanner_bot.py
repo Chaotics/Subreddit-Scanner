7266979f666be02c4b493e0af85eda311b3af441
Anarav <anarav01@gmail.com>
2019-05-25 17:10:36 -0400

    Implement creation of custom feeds

--- scanner_bot.py
+++ scanner_bot.py
@@ -1,12 +1,16 @@
 import configparser
 import praw
+from prawcore.exceptions import Conflict, BadRequest
 import signal
 import sys
+from pprint import pprint
 
-#a signal handler to handle shutdown of the bot
+REDDIT_URL = "https://www.reddit.com"
+
+# a signal handler to handle shutdown of the bot
 def signal_handler(sig, frame):
-        print('Closing bot...')
-        sys.exit(0)
+    print('Closing bot...')
+    sys.exit(0)
 
 
 def login():
@@ -34,41 +38,89 @@ def login():
                        password=password)
 
 
-def readSubreddits():
-    subredditCount = None;
-    while subredditCount is None:
+def read_subreddits():
+    """Method that reads in a list of subreddits from the user to be added to a custom feed"""
+    # starts by asking the user for the number of subreddits they want to add
+    subreddit_count = None
+    while subreddit_count is None:
         try:
-            subredditCount = int(input("Please enter the number of subreddits you want to add to your collection!\n"))
-            break;
+            subreddit_count = int(input("Please enter the number of subreddit(s) you want to add to your collection!\n"))
+            break
+        # if the user input is not a valid number, then it keeps re-prompting the user
         except ValueError:
             print("Please enter an integer value!\n")
-            continue;
-    subredditList = []
-    print("\nPlease enter", subredditCount, "subreddits, each followed by a new line.")
-    for i in range(0, subredditCount):
-        #reads in a line the user enters and stores it in an array
-        subredditList.append(input(""))
-    return subredditList
+            continue
+
+    # next, reads the number of lines for each subreddit and returns the list read
+    subreddit_list = []
+    print("\nPlease enter", subreddit_count, "subreddits, each followed by a new line.")
+    for i in range(0, subreddit_count):
+        # reads in a line the user enters and stores it in an array
+        subreddit_list.append(input(""))
+    return subreddit_list
+
+
+def create_feed(reddit):
+    """Method that creates a custom feed that the user requests"""
+    # starts by prompting the user for a name for the custom feed
+    multi = None
+    feed_name = None
+    while multi is None:
+        feed_name = input("\nWhat would you like to name this new feed? (limit 50 characters)\n")
+        # the name is not allowed to be longer than 50 characters (per Reddit custom feed name specifications)
+        if len(feed_name) > 50:
+            print("The maximum custom feed name length is 50 characters. The name you chose was %d characters long. Please try again" % len(feed_name))
+            continue
+        # and also makes sure that the custom feed created doesn't already exist for the user
+        try:
+            multi = reddit.multireddit.create(display_name=feed_name, subreddits=[])
+            break
+        except Conflict:
+            print("The custom feed with that name already exists on your account. Please choose a different name")
+            continue
+    print("Successfully created an empty custom feed named %s\n" % feed_name)
+
+    # next, the user is prompted for all the subreddits they want to add to the feed
+    subreddit_list = read_subreddits()
+    print("\nAdding the chosen subreddits to the custom feed\n")
+    for subreddit in subreddit_list:
+        # once the list of subreddits is fetched from the user, each of them is added to feed one by one
+        try:
+            multi.add(subreddit)
+            print("Successfully added the subreddit '%s' to the feed" % subreddit)
+        except BadRequest:
+            # if adding the subreddit fails due to a BadRequest, it means that the subreddit provided wasn't valid
+            # so the appropriate error message is printed
+            print("Failed to add the subreddit '%s' to the feed because it is not a valid subreddit" % subreddit)
+
+    # finally, the link to the custom feed is output into the console so they can readily access it
+    print("\nFinished adding the given subreddits to the custom feed. In order to access it, visit:\n%s/%s" % (REDDIT_URL, multi.path))
+
 
 def run_bot():
     """ Method that will run the Subreddit-Scanner bot """
     reddit = login()
-    print("Logged in successfully...\n")
+    print("Logged in successfully...")
+    # runs the program indefinitely
+    while True:
+        # fetches the command
+        command = input("\nWhat would you like to do today?\n"
+                        "1. (c)reate\n"
+                        "2. (q)uit\n")
 
-    subredditList = readSubreddits();
-    print("\nGenerating custom feed for:")
-    for subreddit in subredditList:
-        print(subreddit)
-
-
-    #at this point we have to choose where to store "subreddit list-> either locally in a file or in a database"
-    #we must also take the list and check its validity -> create custom thread
+        # performs the action based on the command given
+        if command == "c" or command == "create":
+            create_feed(reddit)
+        elif command == "q" or command == "quit":
+            sys.exit(0)
+        else:
+            print("Invalid command given! Please choose one of the given options")
 
 
 if __name__ == '__main__':
-    #sets up signal to be recognized by user
+    # sets up signal to be recognized by user
     signal.signal(signal.SIGINT, signal_handler)
     print('To close the bot please press Ctrl + C')
     run_bot()
-    signal.pause() #this is currently here to show the functionality of the signal signal_handler
-    #when we actually make the bot, it will run indefintely so this pause will not be necessary
+    signal.pause()  # this is currently here to show the functionality of the signal signal_handler
+    # when we actually make the bot, it will run indefintely so this pause will not be necessary
